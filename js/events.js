9affcd555395e5cacb2b7de99c44543ea8a13fdf
Aman Adhav <amanadhav@gmail.com>
2019-03-09 21:17:14 -0500

    new commit

--- js/events.js
+++ js/events.js
@@ -11,24 +11,21 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
 		        console.log(ev.detail.intersectedEl, ev.detail);
+            console.log(aEntity1 && intersectedElement === aEntity1);
             if (aEntity1 && intersectedElement === aEntity1) {
-                  const entity = document.querySelector('#animated-model1');
-                  const url = entity.getAttribute('url');
+                  const url = aEntity1.getAttribute('url');
                   window.open(url);
             }
 	          if (aEntity2 && intersectedElement === aEntity2) {
-                  const entity = document.querySelector('#animated-model2');
-                  const url = entity.getAttribute('url');
+                  const url = aEntity1.getAttribute('url');
                   window.open(url);
             }
 	          if (aEntity3 && intersectedElement === aEntity3) {
-                  const entity = document.querySelector('#animated-model3');
-                  const url = entity.getAttribute('url');
+                  const url = aEntity1.getAttribute('url');
                   window.open(url);
             }
 	          if (aEntity4 && intersectedElement === aEntity4) {
-                  const entity = document.querySelector('#animated-model4');
-                  const url = entity.getAttribute('url');
+                  const url = aEntity1.getAttribute('url');
                   window.open(url);
             }
         });
