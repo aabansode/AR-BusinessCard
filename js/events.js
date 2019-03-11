4ab0a02f84fc745a036660fe8c2be674ce9cfc40
Aman Adhav <amanadhav@gmail.com>
2019-03-11 09:36:41 -0400

    new commit

--- js/events.js
+++ js/events.js
@@ -1,34 +1,15 @@
 AFRAME.registerComponent('markerhandler', {
-  init: function() {
-    const animatedMarker = document.querySelector("#animated-marker");
-    // every click, we make our model grow in size :)
-    animatedMarker.addEventListener('click', function(ev, target){
-      const aEntity1 = document.querySelector("#animated-model1");
-      const aEntity2 = document.querySelector("#animated-model2");
-      const aEntity3 = document.querySelector("#animated-model3");
-      const aEntity4 = document.querySelector("#animated-model4");
-      const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-      console.log(ev.detail.intersectedEl, ev.detail);
-      console.log(aEntity1 && intersectedElement === aEntity1);
-      if (aEntity1 && intersectedElement === aEntity1) {
-        const entity = document.querySelector('#animated-model1');
-        const url = entity.getAttribute('url');
-        window.open(url);
-      }
-      if (aEntity2 && intersectedElement === aEntity2) {
-        const entity = document.querySelector('#animated-model2');
-        const url = entity.getAttribute('url');
-        window.open(url);
-      }
-	    if (aEntity3 && intersectedElement === aEntity3) {
-        const entity = document.querySelector('#animated-model3');
-        const url = entity.getAttribute('url');
-        window.open(url);
-      }
-	    if (aEntity4 && intersectedElement === aEntity4) {
-        const entity = document.querySelector('#animated-model4');
-        const url = entity.getAttribute('url');
-        window.open(url);
-      }
-    });
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            console.log(intersectedElement);
+            if (aEntity && intersectedElement === aEntity) {
+                     window.open("https://www.youtube.com/");
+            }
+        });
 }});
